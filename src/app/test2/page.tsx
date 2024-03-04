0d478633bc5685f5c1ed151edf14c3e0f5ce1ad8
r <coviello.royce@gmail.com>
2024-03-03 20:39:39 -0800

    added images

--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -1,8 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/tblhV1WMeUW
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import Link from "next/link"
 import Label from "../components/ui/label"
 import Input from "../components/ui/input"
